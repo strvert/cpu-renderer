bb9ba2e8088ac9aae52d5c40de4e61571883ab4e
strvert <strv@strv.dev>
2022-09-30 05:33:14 +0900

    feat: FacingRatioPainterを非カラーにし、最小の明るさを設定できるようにした

--- Include/Painter/FacingRatioPainter.h
+++ Include/Painter/FacingRatioPainter.h
@@ -2,25 +2,29 @@
 
 #include <algorithm>
 
+#include "Object/Camera/Camera.h"
 #include "Painter/Painter.h"
 #include "Ray/Ray.h"
-#include "Object/Camera/Camera.h"
 
 namespace Raytracer {
 
 class FacingRatioPainter : public Painter {
 public:
-    FacingRatioPainter(const float3& Background = { 0, 0, 0 })
-        : Painter(Background) {};
+    FacingRatioPainter(const float MinBrightness = 0, const float3& Background = { 0, 0, 0 })
+        : MinBrightness(MinBrightness)
+        , Painter(Background) {};
     virtual ~FacingRatioPainter() {};
 
     virtual float3 Paint(const Camera& Camera, const std::optional<Record>& Record) const override
     {
         if (Record && Record->IsFront) {
-            return Record->Color * float3(-std::min<float>(0, dot(Camera.GetDirection(), Record->Normal)));
+            return float3(std::max(MinBrightness, -dot(Camera.GetDirection(), Record->Normal)));
         }
         return Background;
     }
+
+private:
+    float MinBrightness;
 };
 
 }
