a4183755bed1649db4d37a4e1a2f31302a4fc30d
strvert <strv@strv.dev>
2022-10-03 09:54:26 +0900

    feat: Materialクラスを定義

---
+++ Include/Material/Material.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Ray/Ray.h"
+
+namespace Raytracer {
+
+struct SurfaceRecord;
+
+struct SurfaceResponse {
+    bool Valid;
+    Ray Out;
+};
+
+class Material {
+public:
+    Material() { }
+    virtual ~Material() { }
+
+    virtual SurfaceResponse Reflect(const SurfaceRecord& Record, const Ray& InRay) const = 0;
+};
+
+}
