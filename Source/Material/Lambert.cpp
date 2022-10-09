bb1c5c6554ed95404ffb9896359fa1ae08051290
strvert <strv@strv.dev>
2022-10-10 06:30:28 +0900

    doc: 愚かなる自分が忘れないように拡散面に関する修正点をコメントで追加

--- Source/Material/Lambert.cpp
+++ Source/Material/Lambert.cpp
@@ -9,6 +9,7 @@ Lambert::Lambert()
 
 std::optional<Ray> Lambert::Reflect(const float3& Position, const float3& Normal, const Ray& InRay) const
 {
+    // FIXME: バカの実装なので直すこと
     float3 AdHoc = this->RandomVectorInHemisphere(Normal);
     return Ray(Position, dot(Normal, AdHoc) <= 0 ? abs(AdHoc) : AdHoc);
 }
@@ -20,6 +21,7 @@ bool Lambert::ForceShadowRay() const
 
 float3 Lambert::RandomVectorInHemisphere(const float3&) const
 {
+    // FIXME: 明らかの偏りのあるバカの実装なので直すこと(その2)
     return normalize(float3(Distribution(RandomEngine), Distribution(RandomEngine), Distribution(RandomEngine)));
 }
 
